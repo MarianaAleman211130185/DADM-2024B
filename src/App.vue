a7790c15c16f00fa0376d20c6e9f4ae9ecf17b2a
MarianaAleman211130185 <99774985+MarianaAleman211130185@users.noreply.github.com>
2024-09-25 16:00:56 +0000

    ⭐FEAT: Add Vue Add a styles in app Vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <script setup>
+import { ref } from 'vue';
+const header = ref('App lista de compras');
+const items = ref([
+    {id:'1', label:'10 bolillos'},
+    {id:'2',label:'1 lata de frijoles'},
+    {id:'3',label:'1 chela'},
+    {id:'4',label:'1 nutella'},
+    {id:'5',label:'1 lata de atun'},
+    {id:'6',label:'1 pc gamer'}
+]);
 </script>
-
 <template>
-<h1>Shopping Cart</h1>
+<h1>
+    <!--agregamos un icono al codigo a un costado del header-->
+    <i class="material-icons shopping-cart-icon">local_mall</i>
+    {{ header }}
+</h1> 
+<!--ul>li*3 es el lenguaje emet a usar-->
+<ul>
+    <li v-for="item in items" :key="item.id"> 🛒 {{ item.label }}</li>
+</ul>
 </template>
-
 <style scoped>
+.shopping-cart-icon{
+    font-size: 2rem;
+}
 </style>
