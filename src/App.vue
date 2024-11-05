8c64fb2478095d152adf2e7f32b06e5f10a43e99
MarianaAleman211130185 <99774985+MarianaAleman211130185@users.noreply.github.com>
2024-11-05 18:05:25 +0000

    🧪 S03A03 Coffee-Plans

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
-<script setup></script>
-
+<script setup>
+import planItem from './components/plan-item.vue';
+</script>
 <template>
   <header>
     <div style="text-align: center;">
@@ -9,37 +10,16 @@
   </header>
   <div class="content">
     <h1 class="title">Coffee Plans</h1>
-
     <h2 class="subtitle">
       Viajamos por el mundo para encontrar el mejor café de origen único para ti
     </h2>
-
-    <div class="plans">
-      <div class="plan">
-        <div class="description">
-          <span class="title">
-            El Soltero
-          </span>
-        </div>
-      </div>
-      <div class="plan">
-        <div class="description">
-          <span class="title">
-            El Curioso
-          </span>
-        </div>
-      </div>
-      <div class="plan">
-        <div class="description">
-          <span class="title">
-            El adicto
-          </span>
-        </div>
+      <div class="plans">
+      <plan-item>El soltero</plan-item>
+      <plan-item>El Curioso</plan-item>
+      <plan-item>El Adicto</plan-item>
       </div>
     </div>
-  </div>
 </template>
-
 <style scoped>
 header {
   line-height: 1.5;
