bcba13ee58325552fa3f86f7b22a48a29bb183ec
MarianaAleman211130185 <99774985+MarianaAleman211130185@users.noreply.github.com>
2024-11-08 17:00:17 +0000

    ✨S03S04 Componentes Anidados

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,5 +1,5 @@
 <script setup>
-import PlanItem from './PlanItem.vue';
+import PlanItem from './PlanPickerItem.vue';
 import { ref } from 'vue'
 const plans = ref(['El cafetero', 'El chelero', 'El intelectual'])
 </script>
