521c29681efc247410604ff857794f0bfa151be2
MarianaAleman211130185 <99774985+MarianaAleman211130185@users.noreply.github.com>
2024-11-08 16:53:50 +0000

    ⭐S03S04 Componentes

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,14 +1,12 @@
 <script setup>
-import planItem from './plan-item.vue';
-import { ref } from 'vue';
-const plans = ref(['El cafetero', 'El chelero', 'El intelectual']);
+import PlanItem from './PlanItem.vue';
+import { ref } from 'vue'
+const plans = ref(['El cafetero', 'El chelero', 'El intelectual'])
 </script>
 <template>
-<div class="plans">
- <plan-item v-for="plan in plans" :key="plan" :name="plan" />
-</div>
+  <div class="plans">
+    <PlanItem v-for="plan in plans" :key="plan" :name="plan" />
+  </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
