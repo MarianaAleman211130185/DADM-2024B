61ca8f687a8d2317fc24bc080d95f3c7e782642f
MarianaAleman211130185 <99774985+MarianaAleman211130185@users.noreply.github.com>
2024-11-08 16:40:50 +0000

    ⭐S03S04 Componente PlanPicker

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
-import { ref } from 'vue';
-import planItem from './components/plan-item.vue'
-const plans = ref(['El cafetero', 'El chelero', 'El intelectual']);
+import PlanPicker from './components/PlanPicker.vue'; 
 </script>
 <template>
   <header>
@@ -15,9 +13,7 @@ const plans = ref(['El cafetero', 'El chelero', 'El intelectual']);
     <h2 class="subtitle">
       Viajamos por el mundo para encontrar el mejor café de origen único para ti
     </h2>
-    <div class="plans">
-      <plan-item v-for="plan in plans" :key="plan" :name="plan" />
-    </div>
+    <PlanPicker />
   </div>
 </template>
 <style scoped>
---
+++ src/components/PlanPicker.vue
@@ -0,0 +1,14 @@
+<script setup>
+import planItem from './plan-item.vue';
+import { ref } from 'vue';
+const plans = ref(['El cafetero', 'El chelero', 'El intelectual']);
+</script>
+<template>
+<div class="plans">
+ <plan-item v-for="plan in plans" :key="plan" :name="plan" />
+</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
