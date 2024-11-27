0aa90e5e54442e18c57766721377836d8f1d85ef
MarianaAleman211130185 <99774985+MarianaAleman211130185@users.noreply.github.com>
2024-11-27 17:32:15 +0000

    🛠️S03S06 HAOOK Topic template references

--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,12 +1,17 @@
 <script setup>
 import PlanPickerItem from './PlanPickerItem.vue';
-import { onMounted,ref } from 'vue';
-//Creando una referencia reactiva para alamacenar el plan seleccionado
-const selectedPlan = ref(null);
-const plans= ref (['El cafetero', 'El chelero', 'El intelectual']);
-const selectPlan =(name) => {
-  selectedPlan.value = name;
+import { onMounted, ref } from 'vue';
+//Creando una referencia reactiva para almacenar el plan seleccionado
+const selectedPlan = ref(null)
+const plans = ref(['El cafetero', 'El chelero', 'El intelectual'])
+const selectPlan = (name) => {
+  //Almacenar el plan seleccionado
+  selectedPlan.value=name;
+
+  //console.log(name)
 }
+
+const plansWrapper = ref(null);
 
 //Intentando obtener la referencia al elemento .plans
 console.log("Termina de ejecutar el script setup 🎉")
@@ -14,20 +19,21 @@ console.log("Termina de ejecutar el script setup 🎉")
 //Registrando el hook mounted
 onMounted(()=>{
   console.log("El componente PlanPikcker se ha montado")
-  console.log(document.querySelector('plans'))
+  console.log("plansWrapper.value")
 })
 </script>
 
 <template>
-<div class="plans">
-      <PlanPickerItem v-for="plan in plans" 
-      :key="plan"
-      :name="plan"
-      @select="selectPlan"
-      :select-plan="selectedPlan"
-      />
-    </div>
+  <div ref="plansWrapper" class="plans">
+    <PlanPickerItem 
+    v-for="plan in plans" 
+    :key="plan" 
+    :name="plan" 
+    @select="selectPlan"
+    :select-plan="selectedPlan" />
+  </div>
+
 
 </template>
-<style scoped>
-</style>
+
+<style scoped></style>
